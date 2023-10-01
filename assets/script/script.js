33bdc47b53592b54db8a6c0210f92b4d21004f1a
slawkir <slawkir1984@gmail.com>
2023-10-01 22:28:01 +0400

    feat: add keydown enter

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,20 +9,19 @@ const galery = document.querySelectorAll('.galery-item');
 function drawGalary() {
   getSearchInput();
   getData();
-  // drawImg();
 }
 function getSearchInput() { 
   if (searchInput.value.length !== 0) {
     query = searchInput.value;
+  } else { 
+    query = "spring";
   }
   
   url = 'https://api.unsplash.com/search/photos?query=' + `${query}` + '&per_page=30&orientation=landscape&client_id=l78LduGOuh9LnUEnvAY-91u3JZUdsvkD8X0d7OqHvMg';
-  console.log(url);
 }
 async function getData() { 
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
   
   drawImg(data);
 }
@@ -35,7 +34,14 @@ function drawImg(data) {
 
 
 window.addEventListener('load', drawGalary);
+searchInput.focus();
 searchBtn.addEventListener('click', drawGalary);
+document.addEventListener('keydown', (e) => {
+  if (e.code === "Enter") {
+    drawGalary();
+  }
+});
+
 
 
 
